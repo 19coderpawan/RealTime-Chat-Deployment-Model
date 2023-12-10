896570f0254460ad575f0b7c7b78642950262a2c
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-12-10 23:57:59 +0530

    Update Chatsmember.js

--- src/Components/Chatsmember.js
+++ src/Components/Chatsmember.js
@@ -5,49 +5,57 @@ import { onSnapshot,doc } from 'firebase/firestore';
 import { Chatcontext } from '../Context Api/Chatcontext';
 const Chatsmember = () => {
   // This will hold the searched users data and the last message between the curruser and the serached user.
-  const [chatuser,setchatuser]=useState([]);
-  const{currentUser}=useContext(Authcontext);
-  const {dispatch}=useContext(Chatcontext);
-  useEffect(()=>{
-    const getchats=()=>{
+ // ...
 
+
+  const [chatuser, setchatuser] = useState([]);
+  const { currentUser } = useContext(Authcontext);
+  const { dispatch } = useContext(Chatcontext);
+
+  useEffect(() => {
+    const getchats = () => {
       const unsub = onSnapshot(doc(db, "userChat", currentUser.uid), (doc) => {
-        setchatuser(doc.data());
-    });
-    
-    // finally release it or clean up.
-    return()=>{
-      unsub();
-    }
-    }
+        setchatuser(doc.data() || {}); // Set to an empty object if doc.data() is falsy
+      });
+
+      // Cleanup
+      return () => {
+        unsub();
+      };
+    };
 
     currentUser.uid && getchats();
-  },[currentUser.uid]);
+  }, [currentUser.uid]);
+
+  console.log("chatuser data is here:", chatuser);
   console.log(Object.entries(chatuser));
 
-  const handleSelect=(user)=>{
-    dispatch({type:"Change_User",payload:user});
-  }
+  const handleSelect = (user) => {
+    dispatch({ type: "Change_User", payload: user });
+  };
 
   return (
     <>
-     <div className="chatmember">
-    {/* firstly convert the chatuser details from object to array and then apply map for iteration. */}
-    {Object.entries(chatuser)?.sort((a,b)=>b[1].date-a[1].date).map((chat)=>(
-
-     
-      <div className="userchat" key={chat[0]} onClick={()=>handleSelect(chat[1].userinfo)}>
-        <img src={chat[1].userinfo.photoURL} alt="" />
-        <div className="userinfo">
-            <span>{chat[1].userinfo.displayName}</span>
-            <p style={{color:"white",marginTop:"1px"}}>{chat[1].lastMessage?.text}</p>
-        </div>
+      <div className="chatmember">
+        {/* Check if chatuser is an object with keys before using Object.entries */}
+        {Object.keys(chatuser).length > 0 &&
+          Object.entries(chatuser)
+            ?.sort((a, b) => b[1].date - a[1].date)
+            .map((chat) => (
+              <div className="userchat" key={chat[0]} onClick={() => handleSelect(chat[1].userinfo)}>
+                <img src={chat[1].userinfo.photoURL} alt="" />
+                <div className="userinfo">
+                  <span>{chat[1].userinfo.displayName}</span>
+                  <p style={{ color: "white", marginTop: "1px" }}>{chat[1].lastMessage?.text}</p>
+                </div>
+              </div>
+            ))}
       </div>
-     
-    ))}
-     </div>
     </>
-  )
-}
+  );
+};
+
+
+
 
 export default Chatsmember
